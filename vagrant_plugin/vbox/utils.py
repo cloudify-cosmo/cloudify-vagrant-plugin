8704572b39e0bd54a37d3d4e5a550b035cba9184
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-05 23:03:05 +0300

    Add gen ip method definition

--- vagrant_plugin/vbox/utils.py
+++ vagrant_plugin/vbox/utils.py
@@ -3,3 +3,7 @@ def parse_additional_settings(additional_settings, indents):
     for key, value in additional_settings.items():
         string_buffer += '\n{0}{1}= "{2}"'.format('\t'*indents, key, value)
     return string_buffer
+
+
+def gen_ip(ip):
+    raise NotImplementedError
