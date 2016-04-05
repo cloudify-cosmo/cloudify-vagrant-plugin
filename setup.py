b6931ebf8a38055f24e1a76f439fcc0db23689c7
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-05 14:10:53 +0300

    Add dependencies in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-        name='cloudify-vagrant-plugin',
-        version='1.0',
-        author='Gigaspaces',
-        author_email='cosmo-admin@gigaspaces.com',
-        packages=['vagrant_plugin', 'vagrant_plugin/vbox'],
-        license='LICENSE',
-        description='Plugin for running vagrant tasks',
-        install_requires=[
-            'cloudify-plugins-common>=3.3.1',
-        ]
+    name='cloudify-vagrant-plugin',
+    version='1.0',
+    author='Gigaspaces',
+    author_email='cosmo-admin@gigaspaces.com',
+    packages=['vagrant_plugin', 'vagrant_plugin/vbox'],
+    license='LICENSE',
+    description='Plugin for running vagrant tasks',
+    install_requires=[
+        'cloudify-plugins-common>=3.3.1',
+        'jinja2',
+        'vagrant'
+    ]
 )
