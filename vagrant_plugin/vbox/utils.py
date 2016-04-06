82e1d01226195ea93f31cc8274e0b024adf006d6
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-06 10:05:09 +0300

    Add gen random ip method and format

--- vagrant_plugin/vbox/utils.py
+++ vagrant_plugin/vbox/utils.py
@@ -5,9 +5,24 @@ from random import choice, randrange
 def parse_additional_settings(additional_settings, indents):
     string_buffer = ''
     for key, value in additional_settings.items():
-        string_buffer += '\n{0}{1}= "{2}"'.format('\t'*indents, key, value)
+        string_buffer += '\n{0}{1}= "{2}"'.format('\t' * indents, key, value)
     return string_buffer
 
 
 def gen_ip(ip):
-    return str(random.choice(IPNetwork(ip)))
+    if ip:
+        return str(choice(IPNetwork(ip)))
+    else:
+        return gen_rand_ip()
+
+
+def gen_rand_ip():
+    not_valid = [10, 127, 169, 172, 192]
+
+    first = randrange(1, 256)
+    while first in not_valid:
+        first = randrange(1, 256)
+
+    return ".".join(
+        [str(first), str(randrange(1, 256)), str(randrange(1, 256)),
+         str(randrange(1, 256))])
