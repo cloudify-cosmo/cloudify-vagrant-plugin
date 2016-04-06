9db265a18541821246c0d4fdf7abe967b8cd02ca
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-06 21:50:34 +0300

    Move test methods and use util methods

--- vagrant_plugin/tests/local/test_plugin.py
+++ vagrant_plugin/tests/local/test_plugin.py
@@ -1,48 +1,13 @@
-import os
+import utils
 import unittest
 
 from cloudify.test_utils import workflow_test
 
 
-def get_n_dir_back(path, multiplier=1):
-    for i in range(0, multiplier):
-        path = os.path.normpath(os.path.join(path, '..'))
-    return path
-
-
-PACKAGE_PATH = get_n_dir_back(os.getcwd(), multiplier=3)
-PLUGIN_YAML_PATH = os.path.join(PACKAGE_PATH, 'plugin.yaml')
-BLUEPRINT_PATH = \
-    os.path.join(os.path.dirname(os.getcwd()),
-                 os.path.join('blueprint',
-                              'vagrant-command-blueprint.yaml'))
-
-
-@staticmethod
-def get_inputs(test_method):
-    inputs = {'test_my_task': {
-        'vbox': 'file:///home/david/Downloads/precise64.box',
-        'vm_name_prefix': 'test',
-        'vm_cpus': '2',
-        'vm_memory': '1024',
-        'vm_ip_address': '192.0.2.0/23',
-        'additional_vagrant_settings': {},
-        'additional_virtualbox_settings': {},
-        'provision_sets': [
-            {'suffix': 'shell', 'type': 'shell', 'provisions':
-                [
-                    {'inline': 'echo hello world!'},
-                    {'inline': 'echo hello again!'}]}
-        ]
-    }}
-
-    return inputs[test_method]
-
-
 class TestPlugin(unittest.TestCase):
-    @workflow_test(BLUEPRINT_PATH,
-                   resources_to_copy=[PLUGIN_YAML_PATH],
-                   inputs=get_inputs('test_my_task'))
+    @workflow_test(utils.BLUEPRINT_PATH,
+                   resources_to_copy=[utils.PLUGIN_YAML_PATH],
+                   inputs=utils.get_inputs('test_my_task'))
     def test_my_task(self, cfy_local):
         # execute install workflow
         """
