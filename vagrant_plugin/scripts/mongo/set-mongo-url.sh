d260d5311783c85393c2b5cd317245860faf6cec
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-12 14:45:34 +0300

    Delete nodecellar files

--- vagrant_plugin/scripts/mongo/set-mongo-url.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties ip_address)
-ctx source instance runtime_properties mongo_port $(ctx target node properties port)
