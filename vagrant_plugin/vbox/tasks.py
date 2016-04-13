1364a544c5ed2adb67229c10a83e156f4b0e2350
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-13 14:17:13 +0300

    Use vagrant ssh key

--- vagrant_plugin/vbox/tasks.py
+++ vagrant_plugin/vbox/tasks.py
@@ -10,7 +10,9 @@ import vagrant_plugin
 VAGRANTFILE_TEMPLATE = 'Vagrantfile.template'
 VAGRANTFILE_TEMPLATE_PATH = os.path.join(
         os.path.dirname(vagrant_plugin.__file__), 'resources', 'vbox')
-VAGRANTFILE_TMP_DIRECTORY = 'cloudify-vagrant-plugin'
+VAGRANTFILE_TMP_DIRECTORY = 'cloudify-vagrant-plugin-'
+VAGRANT_SSH_PRIVATE_KEY_PATH = os.path.join('.vagrant', 'machines', 'default',
+                                            'virtualbox', 'private_key')
 
 
 @operation
@@ -44,6 +46,8 @@ def start(**kwargs):
     ctx.instance.runtime_properties['ip'] = vm['ip']
     ctx.logger.info('"{0}" ip has been saved to runtime properties'.format(
             vm['ip']))
+    ctx.node.properties['cloudify_agent']['key'] = \
+        os.path.join(output_path, VAGRANT_SSH_PRIVATE_KEY_PATH)
     with open(os.path.join(output_path, 'Vagrantfile'), 'w') as f:
         f.write(template.render(vm=vm))
 
